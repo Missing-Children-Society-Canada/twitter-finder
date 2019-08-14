dcfcc29212e950659024949b344a461a4bfc1af5
Racine Ennis <c-racine.ennis@betach.com>
2019-08-14 09:12:59 -0600

    refactor luis function

--- Luis.cs
+++ Luis.cs
@@ -3,71 +3,35 @@ using System.Threading.Tasks;
 using MCSC.Classes;
 using Newtonsoft.Json;
 using System.Net;
+
 namespace MCSC
 {
-    class LUISHelper
+    internal class LuisHelper
     {
-        static string LuisAppID = Utils.GetEnvVariable("LUISappID");
-        static string LuisKey= Utils.GetEnvVariable("LUISsubscriptionKey");
-        static string LuisEndpoint = Utils.GetEnvVariable("LUISendpoint");
+        private static readonly string LuisAppID = Utils.GetEnvVariable("LUISappID");
+        private static readonly string LuisKey = Utils.GetEnvVariable("LUISsubscriptionKey");
+        private static readonly string LuisEndpoint = Utils.GetEnvVariable("LUISendpoint");
         
-
         ///<summary>
         /// Returns a json string containing the results obtained 
         /// from the LUIS service defined in the env variables
         ///</summary>
-        public async Task<MissingChild> GetLuisResult(string shortSummary, MissingChild missingChild)
+        public static async Task<LuisResult> GetLuisResult(string shortSummary)
         {
             var httpClient = GetHttpClient();
-            string luisUri = LuisEndpoint + LuisAppID + "/?verbose=true&timezoneOffset=-360&q=";
-            string connectionString = $"{luisUri}\"{shortSummary}\"";
-            var response = await httpClient.GetAsync(connectionString);
+            string luisUri = $"{LuisEndpoint}{LuisAppID}?verbose=true&timezoneOffset=-360&q=\"{shortSummary}\"";
+            var response = await httpClient.GetAsync(luisUri);
 
             var contents = await response.Content.ReadAsStringAsync();
             if (response.StatusCode == HttpStatusCode.OK)
             {
                 var luisResult = JsonConvert.DeserializeObject<LuisResult>(contents);
-                foreach(var entity in luisResult.Entities)
-                {
-                    switch (entity.Type)
-                    {
-                        case "Name":
-                        missingChild.Name = entity.EntityFound;
-                        break;
-                        case "City":
-                        missingChild.City = entity.EntityFound;
-                        break;
-                        case "Province":
-                        missingChild.Province = entity.EntityFound;
-                        break;
-                        case "Age":
-                        missingChild.Age = int.Parse(entity.EntityFound);
-                        break;
-                        case "Gender":
-                        missingChild.Gender = entity.EntityFound;
-                        break;
-                        case "Ethnicity":
-                        missingChild.Ethnicity = entity.EntityFound;
-                        break;
-                        case "MissingSince":
-                        missingChild.MissingSince = entity.EntityFound;
-                        break;
-                        case "Height":
-                        missingChild.Height = entity.EntityFound;
-                        break;
-                        case "Weight":
-                        missingChild.Weight = entity.EntityFound;
-                        break;
-                        case "Found":
-                        missingChild.Found = int.Parse(entity.EntityFound);
-                        break;
-                    }
-                }
+                return luisResult;
             }
-            return missingChild;
+            return null;
         }
 
-        static HttpClient GetHttpClient()
+        private static HttpClient GetHttpClient()
         {
             var httpClient = new HttpClient();
             httpClient.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", LuisKey);
