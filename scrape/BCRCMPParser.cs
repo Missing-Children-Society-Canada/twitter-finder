8f17d151fd11d9ad394250a1523b5a574624898f
Racine Ennis <c-racine.ennis@betach.com>
2019-08-14 14:59:22 -0600

    parse BC RCMP content

--- scrape/BCRCMPParser.cs
+++ scrape/BCRCMPParser.cs
@@ -1,10 +1,7 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text.RegularExpressions; 
 using HtmlAgilityPack;
-
 using MCSC.Classes;
+
 namespace MCSC.Parsing
 {
     public class BCRCMPParser : IBodyParse
@@ -14,7 +11,6 @@ namespace MCSC.Parsing
         {
             get
             {
-                
                 // TODO: Test and fix
                 // This website was doing a redirect that was messing up the parsing
                 return "bc.rcmp-grc.gc.ca";
@@ -23,11 +19,41 @@ namespace MCSC.Parsing
 
         public Incident Parse(string body)
         {
-            return new Incident()
+            string summary = "";
+            string shortSummary = "";
+            try
             {
-                // We don't want to set the short summary because then luis will parse it!
-                // ShortSummary = body,
-                Summary = body
+                // Load the document
+                var document = new HtmlDocument();
+                document.LoadHtml(body);
+
+                var infoNodes = document.DocumentNode.SelectNodes("//main");
+
+                if (infoNodes != null)
+                {
+                    // Pull only the inner HTML
+                    foreach (HtmlNode n in infoNodes)
+                    {
+                        shortSummary = shortSummary + n.InnerText;
+                    }
+                    if (!String.IsNullOrEmpty(shortSummary))
+                    {
+                        // Got something meaningful from short summary!
+                        summary = shortSummary;
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                // TODO: Proper Error handling!!!
+                summary = body;
+            }
+
+            // return the results
+            return new Incident
+            {
+                Summary = summary,
+                ShortSummary = shortSummary,
             };
         }
     }
