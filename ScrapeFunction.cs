bcd01aff2188b18cfb1270f8bb925125055b4fd1
Racine Ennis <c-racine.ennis@betach.com>
2019-08-14 14:58:48 -0600

    refactor summary cleanup. add parser factory pattern

--- ScrapeFunction.cs
+++ ScrapeFunction.cs
@@ -1,13 +1,9 @@
-using System;
 using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
 using MCSC.Parsing;
 using MCSC.Classes;
 using Newtonsoft.Json;
 using System.Collections.Generic;
-using System.Net;
-using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace MCSC
@@ -26,24 +22,22 @@ namespace MCSC
             var scrapedTweets = new List<LuisInput>();
             foreach(var tweet in listOfTweets)
             {
-                var luisInput = new LuisInput()
+                var luisInput = new LuisInput
                 {
                     SourceUrl = tweet.SourceUrl,
                     TwitterProfileUrl = tweet.TwitterProfileURL,
-                    TweetUrl = tweet.TweetUrl,
+                    TweetUrl = tweet.TweetUrl
                 };
 
                 if(!string.IsNullOrEmpty(luisInput.SourceUrl))
                 {
-                    // If Twitter is the source url then don't bother parsing
-                    if (!luisInput.SourceUrl.Contains("https://twitter.com"))
-                    {
-                        var reference = new Reference(luisInput.SourceUrl);
-                        var incident = reference.Load();
+                    log.LogInformation($"Loading external reference into scraper '{luisInput.SourceUrl}'.");
 
-                        luisInput.ShortSummary = incident.ShortSummary;
-                        luisInput.Summary = incident.Summary;
-                    }
+                    var reference = new Reference(luisInput.SourceUrl, log);
+                    var incident = reference.Load();
+
+                    luisInput.ShortSummary = incident.ShortSummary;
+                    luisInput.Summary = incident.Summary;
                 }
                 
                 scrapedTweets.Add(luisInput);
--- scrape/Reference.cs
+++ scrape/Reference.cs
@@ -2,22 +2,24 @@ using System;
 using HtmlAgilityPack;
 using System.Linq;
 using System.Net;
+using System.Text.RegularExpressions;
 using MCSC.Classes;
+using Microsoft.Extensions.Logging;
 
 namespace MCSC.Parsing
 {
     public class Reference
     {
         private readonly string uri;
-        private readonly Parser parser;
+        private readonly ILogger _logger;
 
-        public Reference(string uri)
+        public Reference(string uri, ILogger logger)
         {
-            this.uri = uri.ToLower();
-            this.parser = new Parser(uri);
+            this.uri = uri;
+            _logger = logger;
         }
 
-        public string Body()
+        private string Body()
         {
             try
             {
@@ -34,7 +36,7 @@ namespace MCSC.Parsing
             }
             catch (Exception e)
             {
-                // TO DO: Error Logging something went wrong
+                _logger.LogError(e, "Error extracting body from external reference document.");
                 return string.Empty;
             }
         }
@@ -44,10 +46,117 @@ namespace MCSC.Parsing
             var body = this.Body();
             if (String.IsNullOrEmpty(body))
             {
-                // TO DO: Error Logging something went wrong
+                _logger.LogInformation("External reference body is empty.");
                 return new Incident();
             }
-            return this.parser.Parse(body);
+
+            string cutdownbody = "";
+            try
+            {
+
+                // Load the document
+                var document = new HtmlDocument();
+                document.LoadHtml(body);
+
+                // The script and Style tags are never useful so remove them before more specific processing
+                document.DocumentNode.Descendants()
+                    .Where(n => n.Name == "script" || n.Name == "style")
+                    .ToList()
+                    .ForEach(n => n.Remove());
+
+                // Cutdown body is the body just without Style and script tags
+                cutdownbody = document.DocumentNode.InnerHtml;
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, "Error cutting script and style tags from external reference document.");
+                cutdownbody = body;
+            }
+
+
+            var parser = ParserFactory.Instance.BuildParser(this.uri);
+            var incident = parser.Parse(cutdownbody);
+
+            var shortSummary = "";
+            var summary = "";
+
+            if (!String.IsNullOrEmpty(incident.ShortSummary))
+            {
+                // If the short summary is available then cut it down to be optimized for LUIS
+                shortSummary = this.ShortSummaryCleanUp(incident.ShortSummary);
+            }
+
+            if (!String.IsNullOrEmpty(incident.Summary))
+            {
+                // If the summary is available then cut it down to make it more human readable
+                summary = this.SummaryCleanUp(incident.Summary);
+            }
+
+            return new Incident
+            {
+                ShortSummary = shortSummary,
+                Summary = summary
+            };
+        }
+
+        // Clean up for the summary
+        // The summary is the human readable content that is displayed in the ESRI portal 
+        public string SummaryCleanUp(string body)
+        {
+            //Console.WriteLine("Inside Clean up!!! \n");
+            // Remove all tags
+            string result = Regex.Replace(body, "<.*?>", " ");
+
+            result = WebUtility.HtmlDecode(result);
+
+            result = Regex.Replace(result, @"[#]+|\\s+|\t|\n|\r", " ");
+
+            // Remove all extra spaces
+            var options = RegexOptions.Compiled | RegexOptions.Multiline | RegexOptions.CultureInvariant;
+            Regex regex = new Regex("[ ]{2,}", options);
+            result = regex.Replace(result, " ");
+
+            result = result.Trim();
+            return result;
+        }
+
+        // Clean up for the ShortSummary
+        // The short summary is the shortened version of the summary that is optimized to be processed by LUIS
+        public string ShortSummaryCleanUp(string body)
+        {
+            // Remove all tags
+            string result = Regex.Replace(body, "<.*?>", " ");
+            result = result.ToLower();
+            result = result.Replace("&nbsp;", " ");
+            result = result.Replace("&quot;", " ");
+            result = result.Replace("&ndash;", " ");
+            result = result.Replace("&rsquo;", " ");
+            result = result.Replace("&#8217;", "'");
+            result = result.Replace("&#8243;", "\"");
+
+            string[] FillerWords = {"a", "and", "the", "on","or", "at", "was", "with", "contact", "nearest", "detachment",
+             "failed", "investigations", "anyone", "regarding", "approximately", "in", "is", "uknown", "time", "of", "any", "to", "have", "seen",
+             "if", "UnknownClothing", "applicable", "UnknownFile", "it", "unknownclothing", "information","unknownfile", "police", "service", "call", "crime",
+             "stoppers", "from", "by", "all", "also", "that", "his", "please", "been", "this", "concern", "they","are","they","as","had","wearing",
+             "color", "colour", "shirt", "pants","be", "believed", "guardians", "network", "coordinated", "response", "without","complexion",
+             "has", "for", "well-being", "there", "included", "release", "picture", "family", "younger", "shorts", "described", "reported", "police", "officer", 
+             "public", "attention", "asked", "live", "own", "complexity", "victimize", "children", "child", "nations", "when", "person", "jeans", "shoes", "thin",
+             "area", "road", "criminal", "investigation", "division", "concerned", "concern", "build", "assistance", "seeking", "locate", "locating", "stripe",
+             "stripes", "straight", "requesting", "request", "requests", "facebook", "twitter", "avenue", "road", "street", "large", "long", "tiny", "hoodie",
+             "leggings", "sweater", "jacket", "boots", "tennis shoes", "leather", "worried", "backpack", "purse", "whereabouts", "unknown", "help",
+             "monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday", "block", "crossing", "harm", "not", 
+             "danger", "described", "vunerable", "picture", "friend", "thinks", "things", "media", "year", "about", "providers", "cash", "unsuccessful", "attempts", 
+             "accurately", "slimmer", "slightly", "however", "nevertheless", "nike", "adidas", "puma", "joggers"};
+
+            result = Regex.Replace(result, @"\b" + string.Join("\\b|\\b", FillerWords) + "\\b", "");
+            result = Regex.Replace(result, @"[&.;()@#~_]+|\\s+|\t|\n|\r", " ");
+
+            // Remove all extra spaces
+            var options = RegexOptions.Compiled | RegexOptions.Multiline | RegexOptions.CultureInvariant;
+            Regex regex = new Regex("[ ]{2,}", options);
+            result = regex.Replace(result, " ");
+
+            return result.Trim();
         }
     }
 
